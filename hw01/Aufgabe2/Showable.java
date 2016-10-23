2925b21d8956ce6344da2ced0ff0bcfcc0137865
Pfeilsticker <adam.pfeilsticker@student.uni-tuebingen.de>
2016-10-23 23:47:00 +0200

    uploat 8000

--- hw01/Aufgabe2/Showable.java
+++ hw01/Aufgabe2/Showable.java
@@ -1,3 +1,4 @@
+package main;
 
 
 public abstract class Showable<T>{
@@ -16,23 +17,28 @@ public abstract class Showable<T>{
 	}
 
 	
-	
-	public static <T> String show(T t){
-		if(t instanceof Pair){
-			return("("+ Showable.show(((Pair) t).getKey())+","+Showable.show(((Pair) t).getValue())+")");
-		}else if(t instanceof String){
-			return (String) t;
-		}
-		else if(t instanceof Integer){
-			return Integer.toString((Integer) t);
-		}else
-		return "Objekt kein String, Integer oder Pair"; 
-		
-	}
-	/* Aus irgend einem Grund funktioniert das Bounding nicht, wenn ich die Elemente eines Paares rekursive mit show aufrufe
-
+	/*
+	 Aus irgend einem Grund funktioniert das Bounding bei den Rekursiven aufrufen in der Pair Methode, deshalb habe
+	 ich das etwas unschön mit den if abfragen gelöst
+	 */
 	public static <T extends Pair> String show(T t){
-		return("("+ Showable.show(((Pair) t).getKey())+","+Showable.show(((Pair) t).getValue())+")");
+	
+		String a ="a";
+		String b ="b";
+		if(t.getKey() instanceof Integer)
+			a = show((Integer) t.getKey());
+		else if(t.getKey() instanceof String)
+			a = show((String) t.getKey());
+		else if(t.getKey() instanceof Pair)
+			a = show((Pair) t.getKey());	
+		
+		if(t.getValue() instanceof Integer)
+			b = show((Integer) t.getValue());
+		else if(t.getValue() instanceof String)
+			b = show((String) t.getValue());
+		else if(t.getValue() instanceof Pair)
+			b = show((Pair) t.getValue());
+		return("("+ a +","+b+")");
 	}
 	public static <T extends String> String show(T t){
 		return (String) t;
@@ -43,5 +49,19 @@ public abstract class Showable<T>{
 	public static <T > String show(T t){
 		System.out.println("t" +t.getClass());
 		return("error");
-	}	*/
+	}	
+	
+	/*
+	public static <T> String show(T t){
+		if(t instanceof Pair){
+			return("("+ Showable.show(((Pair) t).getKey())+","+Showable.show(((Pair) t).getValue())+")");
+		}else if(t instanceof String){
+			return (String) t;
+		}
+		else if(t instanceof Integer){
+			return Integer.toString((Integer) t);
+		}else
+		return "Objekt kein String, Integer oder Pair"; 
+	
+	}*/	
 }
